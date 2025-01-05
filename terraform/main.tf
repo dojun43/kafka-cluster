68282f61ff8a178aeb84579d75af1b1348b043c1
dojunhee <dodo@dojunheeui-MacBookPro.local>
2025-01-05 16:05:17 +0900

    add 2 node

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,6 +3,8 @@ resource "google_compute_network" "kafka_network" {
   name = "kafka-network"
 }
 
+
+
 # 고정 ip
 resource "google_compute_address" "kafka-node1-static-ip" {
   name   = "kafka-node1-static-ip"
@@ -10,7 +12,35 @@ resource "google_compute_address" "kafka-node1-static-ip" {
   region  = var.region
 }
 
+resource "google_compute_address" "kafka-node2-static-ip" {
+  name   = "kafka-node2-static-ip"
+  project = var.project
+  region  = var.region
+}
+
+resource "google_compute_address" "kafka-node3-static-ip" {
+  name   = "kafka-node3-static-ip"
+  project = var.project
+  region  = var.region
+}
+
+
+
 # 방화벽 정책
+resource "google_compute_firewall" "allow-ssh" {
+  name    = "allow-ssh"
+  project = var.project
+  network = google_compute_network.kafka_network.name
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  source_ranges = ["0.0.0.0/0"] 
+  target_tags   = ["allow-ssh"]
+}
+
 resource "google_compute_firewall" "allow-kafka-ports" {
   name    = "allow-kafka-ports"
   project = var.project
@@ -18,12 +48,14 @@ resource "google_compute_firewall" "allow-kafka-ports" {
 
   allow {
     protocol = "tcp"
-    ports    = ["2181", "9092", "9000"]
+    ports    = ["29092", "9000"]
   }
 
   source_ranges = ["0.0.0.0/0"] 
   target_tags   = ["allow-kafka-ports"]
 }
+
+
 
 # disk
 resource "google_compute_disk" "kafka-node1-disk" {
@@ -32,6 +64,22 @@ resource "google_compute_disk" "kafka-node1-disk" {
   size  = 100 
   type  = "pd-standard"
 }
+
+resource "google_compute_disk" "kafka-node2-disk" {
+  name  = "kafka-node2-disk"
+  zone  =  var.zone
+  size  = 100 
+  type  = "pd-standard"
+}
+
+resource "google_compute_disk" "kafka-node3-disk" {
+  name  = "kafka-node3-disk"
+  zone  =  var.zone
+  size  = 100 
+  type  = "pd-standard"
+}
+
+
 
 # VM
 resource "google_compute_instance" "kafka-node1" {
@@ -59,5 +107,61 @@ resource "google_compute_instance" "kafka-node1" {
 
   metadata_startup_script = file("startup_script.sh")
 
-  tags = ["allow-kafka-ports"]
+  tags = ["allow-ssh", "allow-kafka-ports"]
+}
+
+resource "google_compute_instance" "kafka-node2" {
+  name         = "kafka-node2"
+  machine_type = "e2-small"
+  zone         = var.zone
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-os-cloud/ubuntu-2204-lts"
+    }
+  }
+
+  attached_disk {
+    source      = google_compute_disk.kafka-node2-disk.id
+    device_name = "kafka-node2-disk"
+  }
+
+  network_interface {
+    network = google_compute_network.kafka_network.name
+    access_config {
+      nat_ip = google_compute_address.kafka-node2-static-ip.address
+    }
+  }
+
+  metadata_startup_script = file("startup_script.sh")
+
+  tags = ["allow-ssh", "allow-kafka-ports"]
+}
+
+resource "google_compute_instance" "kafka-node3" {
+  name         = "kafka-node3"
+  machine_type = "e2-small"
+  zone         = var.zone
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-os-cloud/ubuntu-2204-lts"
+    }
+  }
+
+  attached_disk {
+    source      = google_compute_disk.kafka-node3-disk.id
+    device_name = "kafka-node3-disk"
+  }
+
+  network_interface {
+    network = google_compute_network.kafka_network.name
+    access_config {
+      nat_ip = google_compute_address.kafka-node3-static-ip.address
+    }
+  }
+
+  metadata_startup_script = file("startup_script.sh")
+
+  tags = ["allow-ssh", "allow-kafka-ports"]
 }
